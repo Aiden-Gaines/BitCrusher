788896bcde2e87acadf3ec6abe041893215fe2d9
Aiden Gaines <aidengaines@yahoo.com>
2021-09-13 19:18:15 -0500

    Startup animation. Started work on game loop

--- app/index.js
+++ app/index.js
@@ -1,18 +1,106 @@
 import * as document from "document";
 // import clock from "clock";
 // import { preferences } from "user-settings";
-// import * as util from "../common/utils";
+import * as utils from "../common/utils";
 
+let progress = 0;
+let frame = 0;
+let direction = 1;
 const brickArr = [];
+const shownBricks = [];
+const activeBricks = [[0, 8], [1, 8], [2, 8]];
 // Values based on SVG, will not automatically update when one is changed
 const brickSize = 30;
 const brickBorder = 2;
 const brickOffset = 6;
-const rowCount = 5;
+const rowCount = 9;
 const colCount = 9;
-// const shownBricks = [];
-// const activeBricks = [[8, 8]];
 
+// Functions
+function getBrick(brickCoord) {
+	return brickArr[brickCoord[1]][brickCoord[0]];
+}
+
+function show(brick) {
+	if (brick.class == undefined) {
+		getBrick(brick).class = "";
+	} else {
+		brick.class = "";
+	}
+}
+
+function hide(brick) {
+	if (brick.class == undefined) {
+		getBrick(brick).class = "hide";
+	} else {
+		brick.class = "hide";
+	}
+}
+
+function flip(brick) {
+	switch (brick.class) {
+		case ("hide"):
+			show(brick);
+			break;
+		case (""):
+			hide(brick);
+			break;
+	}
+}
+
+function getNewActiveBricks() {
+	const newActiveBricks = [];
+	for (const brick of activeBricks) {
+		newActiveBricks.push([brick[0] + direction, brick[1]])
+	}
+	return newActiveBricks;
+}
+
+function mainAnim() {
+	if (frame % 10 == 0) {
+		const newActiveBricks = getNewActiveBricks();
+		console.log(newActiveBricks);
+		const switchBricks = utils.arrSymDiff(activeBricks, newActiveBricks);
+		console.log(utils.arrSymDiff([1, 2, 3], [2, 3, 4]));
+		console.log([[1, 8], [2, 8]].indexOf([1, 8]));
+		for (const brick of switchBricks) {
+			flip(brick);
+		}
+	}
+	animate();
+}
+
+function convert1Dto2D(index) {
+	return [index % 9, Math.floor(index / 9)];
+}
+
+let setupIndex = 0;
+
+function setup() {
+	// hide all bricks
+	if (frame % 1 == 0) {
+		hide(convert1Dto2D(setupIndex));
+		setupIndex++;
+	}
+	if (setupIndex >= rowCount * colCount) {
+		activeBricks.forEach(show);
+		progress++;
+	}
+	animate();
+}
+
+function animate() {
+	switch (progress) {
+		case (0): break;
+		case (1):
+			requestAnimationFrame(setup);
+			break;
+		case (2):
+			requestAnimationFrame(mainAnim);
+			break;
+	}
+	frame++;
+}
 
 // MAIN
 // Get each brick
@@ -30,7 +118,8 @@ for (let y = 0; y < rowCount; y++) {
     let brick = brickArr[y][x];
     brick.x = ((brickSize + brickBorder) * x) + brickOffset;
     brick.y = ((brickSize + brickBorder) * y) + brickOffset;
-    brick.class = "";
   }
 }
 
+progress++;
+animate();
--- common/utils.js
+++ common/utils.js
@@ -5,3 +5,11 @@ export function zeroPad(i) {
   }
   return i;
 }
+
+// Find the difference between two sets
+export function arrSymDiff(arr1, arr2) {
+	return arr1
+		// .filter(x => !arr2.includes(x))
+		.filter(x => -1 == arr2.indexOf(x))
+		.concat(arr2.filter(x => -1 == arr1.indexOf(x)));
+}
