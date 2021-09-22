50ff1afa649838879848629fce80c923d71b3f42
Nick Tonjum <nicholas_tonjum@hotmail.com>
2021-09-22 15:23:48 -0500

    re-enabled and changed start button on main menu

--- app/MainScreen.js
+++ app/MainScreen.js
@@ -5,9 +5,11 @@ var myImage = document.getElementById("MainScreen");
 myImage.x=80;
 myImage.y=20;
 
+
 function startbutton(){
     let button = document.getElementById("homebutton");
     button.onactivate = function(evt) {
         button.text = "Clicked";
+        console.log("button pressed");
     }
 }
