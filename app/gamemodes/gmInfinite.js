9bb858696b1d39dd4da30fcf82fef669797237ae
Nick Tonjum <nicholas_tonjum@hotmail.com>
2021-09-28 15:21:24 -0500

    stuff and things

--- app/gamemodes/gmInfinite.js
+++ app/gamemodes/gmInfinite.js
@@ -78,11 +78,41 @@ function screenClick(evt) {
 	calculateCurrentSpeed();
 }
 
+function hslToHex(h, s, l) {
+	h /= 360;
+	s /= 100;
+	l /= 100;
+	let r, g, b;
+	if (s === 0) {
+	  r = g = b = l; // achromatic
+	} else {
+	  const hue2rgb = (p, q, t) => {
+		if (t < 0) t += 1;
+		if (t > 1) t -= 1;
+		if (t < 1 / 6) return p + (q - p) * 6 * t;
+		if (t < 1 / 2) return q;
+		if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+		return p;
+	  };
+	  const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+	  const p = 2 * l - q;
+	  r = hue2rgb(p, q, h + 1 / 3);
+	  g = hue2rgb(p, q, h);
+	  b = hue2rgb(p, q, h - 1 / 3);
+	}
+	const toHex = x => {
+	  const hex = Math.round(x * 255).toString(16);
+	  return hex.length === 1 ? '0' + hex : hex;
+	};
+	return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+  }
+
+const myGradient = document.getElementById('bgGradient');
+
 export function gmInfiniteSetup(status, { rowCount }) {
 	const myButton = document.getElementById('button-screenwide');
-	const myGradient = document.getElementById('bgGradient');
-	myGradient.gradient.colors.c1 = "red";
-	myGradient.gradient.colors.c2 = "lime";
+	myGradient.gradient.colors.c1 = hslToHex(1,100,50);
+	myGradient.gradient.colors.c2 = hslToHex(100,100,50);
 	localRowCount = rowCount;
 	myButton.onclick = screenClick;
 
